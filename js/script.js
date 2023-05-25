98d958a21c75e38dee0015e4d1240a4fe52a58d9
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-25 08:51:53 -0500

    Add initial div to template

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container')
+const gridBox = document.createElement('div')
+gridBox.classList.add('square')
+gridBox.textContent = 'butt'
+container.appendChild(gridBox)
+console.log('butt')
