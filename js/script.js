482855eb04834e29f050e6e243657cb89de34020
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-25 11:19:00 -0500

    Add JS to put 16 rows of divs 16 divs wide

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-const container = document.querySelector('.container')
+
+const pad = document.querySelector('.pad'); //set the pad class to the const pad
+
+for (let r = 1; r < 17; r++){
+    const gridRow = document.createElement('div'); //create a div that is the row
+    gridRow.classList.add('container'); //add the container class to that div
+    pad.appendChild(gridRow); //put the div with the class of container in the pad.
+
+
 
 
     for (let i = 1; i < 17; i++){  
     const gridBox = document.createElement('div')
     gridBox.classList.add('square')
     gridBox.textContent = i
-    container.appendChild(gridBox)
+    gridRow.appendChild(gridBox)
+}
 }
