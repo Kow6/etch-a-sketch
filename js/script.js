c66e0e5b3a78cf5f3f5e3565d9dcf3825b4a7b0f
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-25 13:24:45 -0500

    Add button to allow user to enter a new grid about

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const pad = document.querySelector('.pad'); //set the pad class to the const pad
 
 
+//Define funtion to create grid
 
+function createGrid(squaresPerSide){
 
-for (let r = 0; r < 16; r++){ //loop to to all the rows
+//check and remove for nodes KEEP THIS
+while (pad.hasChildNodes()){
+pad.removeChild(pad.firstChild)
+}
+//end remove
+
+for (let r = 0; r < squaresPerSide; r++){ //loop to to all the rows
     const gridRow = document.createElement('div'); //create a div that is the row
     gridRow.classList.add('row'); //add the container class to that div
     pad.appendChild(gridRow); //put the div with the class of container in the pad.
@@ -11,16 +19,36 @@ for (let r = 0; r < 16; r++){ //loop to to all the rows
 
 
 
-    for (let i = 0; i < 16; i++){  //loop to do all the columns
+    for (let i = 0; i < squaresPerSide; i++){  //loop to do all the columns
     const gridBox = document.createElement('div');
     gridBox.classList.add('square');
     gridRow.appendChild(gridBox);
 }
 }
 
+//event listener for mouseover to add a css class to make dark
 const boxes = document.querySelectorAll('.square')
 boxes.forEach((boxes) =>{
 boxes.addEventListener('mouseover', () => {
 boxes.classList.add('hover') 
 } )
+})
+
+}
+//End Function
+
+
+
+createGrid(16) //run funtion to create the grid
+
+//Add Event Listener on the button
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+
+let squarePerSide = prompt('Please enter the number of squares per side.')
+if (squarePerSide < 100){createGrid(squarePerSide)
+}
+else (alert('Choose a number below 100, please')
+)
+
 })
