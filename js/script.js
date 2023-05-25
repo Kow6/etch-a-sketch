b4b20e973ff72b85931fb89ab1e58121805fe709
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-25 08:59:53 -0500

    Add JS to put 16 div's on the page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container')
-const gridBox = document.createElement('div')
-gridBox.classList.add('square')
-gridBox.textContent = 'butt'
-container.appendChild(gridBox)
-console.log('butt')
+
+
+    for (let i = 1; i < 17; i++){  
+    const gridBox = document.createElement('div')
+    gridBox.classList.add('square')
+    gridBox.textContent = i
+    container.appendChild(gridBox)
+}
