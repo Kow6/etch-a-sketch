d4dd484be4caae090d67dc49c47b28adfc723046
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-25 12:17:04 -0500

    Add js to make grid boxes change class when mouse over

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const pad = document.querySelector('.pad'); //set the pad class to the const pad
 
-for (let r = 1; r < 17; r++){
+
+
+
+for (let r = 0; r < 16; r++){ //loop to to all the rows
     const gridRow = document.createElement('div'); //create a div that is the row
     gridRow.classList.add('row'); //add the container class to that div
     pad.appendChild(gridRow); //put the div with the class of container in the pad.
@@ -8,10 +11,16 @@ for (let r = 1; r < 17; r++){
 
 
 
-    for (let i = 1; i < 17; i++){  
-    const gridBox = document.createElement('div')
-    gridBox.classList.add('square')
-    gridBox.textContent = i
-    gridRow.appendChild(gridBox)
+    for (let i = 0; i < 16; i++){  //loop to do all the columns
+    const gridBox = document.createElement('div');
+    gridBox.classList.add('square');
+    gridRow.appendChild(gridBox);
 }
 }
+
+const boxes = document.querySelectorAll('.square')
+boxes.forEach((boxes) =>{
+boxes.addEventListener('mouseover', () => {
+boxes.classList.add('hover') 
+} )
+})
